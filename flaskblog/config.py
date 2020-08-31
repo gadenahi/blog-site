9c831c0992db8d03125edafcd15cee4f65e71156
gadenahi@gmail.com <gadenahi@gmail.com>
2020-08-31 14:09:54 -0700

    updated config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,12 +11,13 @@ class Config:
     """
     # move to  .bash_profile
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     # MAIL_USERNAME = os.environ.get('EMAIL_USER') # use your own account
     # MAIL_PASSWORD = os.environ.get('EMAIL_PASS') # use your own account
-    MAIL_USERNAME = 'gadenahi@gmail.com'
-    MAIL_PASSWORD = 'xvaottzrmtfvuuse'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # https://stackoverflow.com/questions/28209414/sending-a-mail-from-flask-mail-smtpsenderrefused-530
